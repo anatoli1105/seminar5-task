ed20accf076d90380695d9cd2564d32c5cc58163
anatoli1105 <99749802+anatoli1105@users.noreply.github.com>
2022-03-29 09:38:32 +0300

    Update Program.cs

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,20 +1,19 @@
 ﻿// Задача 3: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 //[3, 7, 22, 2, 78] -> 76
 //int[] array = Rand(8);
-//int evenNumber = Sum(arr,false);
-//int oddNumber = Sum(array,true);
-int[] array = Rand(8);
 
-int[] Rand(int lenght)
+
+double[] array = Rand(8);
+double[] Rand(int lenght)
 {
-    int[] Set = new int[lenght];
+    double[] Set = new double[lenght];
     for(int i = 0;i < lenght;i++)
     {
         Set[i] = new Random().Next(0,30);
     }
     return Set;
 }
-void PrintArr(int[] Mas)
+void PrintArr(double[] Mas)
 {
     int count = Mas.Length;
     for(int pos = 0; pos < count;pos++)
@@ -22,10 +21,10 @@ void PrintArr(int[] Mas)
         Console.Write(Mas[pos] + " ,");
     }
 }
-int SumMax(int[] numb)
+double SumMax(double[] numb)
 {
-    int lenght = numb.Length;
-    int max = 0; 
+    double lenght = numb.Length;
+    double max = 0; 
     for(int i = 0;i < lenght;i++)
     {
         if(max < numb[i])
@@ -36,10 +35,10 @@ int SumMax(int[] numb)
     }
     return max;
 }
-int SumMin(int[] numb)
+double SumMin(double[] numb)
 {
-    int lenght = numb.Length;
-    int min = 1000; 
+    double lenght = numb.Length;
+    double min = 1000; 
     for(int i = 0;i < lenght;i++)
     {
         if(numb[i] < min)
@@ -59,3 +58,4 @@ Console.WriteLine();
 Console.WriteLine($"min- {SumMin(array)}");
 Console.WriteLine();
 Console.WriteLine($"разница между максимальным и минимальным элементов массива -> {SumMax(array) - SumMin(array)} ");
+
